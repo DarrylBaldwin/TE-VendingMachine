733758ef803c93bcb5f6e51a4fabddc01b15ca5d
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-07 12:17:00 -0500

    Add PurchaseMenu() in UserInterface.cs

--- Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -48,5 +48,44 @@ namespace Capstone.Classes
             }
 
         }
+
+        void PurchaseMenu()
+        {
+            bool done = false;
+            int choice = 0;
+            while(!done)
+            {
+                Console.WriteLine("(1) Feed Money");
+                Console.WriteLine("(2) Select Product");
+                Console.WriteLine("(3) Finish Transaction");
+                //TODO: create Console.WriteLine($"\nCurrent Money Provided: {vendingMachine.CurrentMoney}");
+                Console.Write("\nPlease choose an option: ");
+
+                string userInput = Console.ReadLine();
+                try
+                {
+                    choice = int.Parse(userInput);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Invalid input\n{e.Message}");
+                    Console.WriteLine("Press enter to return to the main menu");
+                    Console.ReadLine();
+                }
+                if (choice == 1)
+                {
+                    //TODO: create vendingMachine.FeedMoney();
+                }
+                else if (choice == 2)
+                {
+                    //TODO: create SelectProductMenu();
+                }
+                else if (choice == 3)
+                {
+                    //TODO: create FinishTransaction();
+                    done = true;
+                }
+            }
+        }
     }
 }
