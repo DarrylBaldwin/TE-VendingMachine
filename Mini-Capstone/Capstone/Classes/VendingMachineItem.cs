5da873992b810c435745641d58f21ebf5ccb7bb7
Darryl Baldwin <darrylbaldwin01@gmail.com>
2019-02-07 13:02:27 -0500

    Test method and passing code for VendingMachineItem class Consumed_Message_Test_A

--- Mini-Capstone/Capstone/Classes/VendingMachineItem.cs
+++ Mini-Capstone/Capstone/Classes/VendingMachineItem.cs
@@ -6,6 +6,41 @@ namespace Capstone.Classes
 {
     public class VendingMachineItem
     {
+        public string Name { get; }
+        public decimal Price { get; }
+        public string Slot { get; }
+        public int QuantityRemaining { get; private set; }
+        public string ConsumedMessage
+        {
+            get
+            {
+                if (Slot.Contains('A'))
+                {
+                    return "Crunch Crunch, Yum";
+                }
+                else if (Slot.Contains('B')) 
+                {
+                    return "Munch Munch, Yum";
+                }
+                else if (Slot.Contains('C'))
+                {
+                    return "Glug Glug, Yum";
+                }
+                else if (Slot.Contains('D'))
+                {
+                    return "Chew Chew, Yum";
+                }
+                return "unknown";
+            }
+        }
 
+        public VendingMachineItem(string name, decimal price, string slot)
+        {
+            Name = name;
+            Price = price;
+            Slot = slot;
+            QuantityRemaining = 5;
+        }
     }
 }
+
---
+++ Mini-Capstone/CapstoneTests/VendingMachineItemTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Capstone.Classes.Tests
+{
+    [TestClass()]
+    public class VendingMachineItemTests
+    {
+        VendingMachineItem item;
+
+
+        [TestMethod]
+        public void ConsumedMessageTest_A_Should_Be_Crunch_Crunch()
+        {
+            item = new VendingMachineItem("Potato Crisps", 3.05M, "A1");
+
+            Assert.AreEqual("Crunch Crunch, Yum", item.ConsumedMessage);
+
+        }
+    }
+}
