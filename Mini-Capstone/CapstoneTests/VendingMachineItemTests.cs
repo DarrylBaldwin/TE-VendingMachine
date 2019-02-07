a118493a6c2db3f661de18557de139f627ef4627
Darryl Baldwin <darrylbaldwin01@gmail.com>
2019-02-07 13:07:19 -0500

    Test method and passing code for VendingMachine Item class Consumed_Message_Test_B

--- Mini-Capstone/CapstoneTests/VendingMachineItemTests.cs
+++ Mini-Capstone/CapstoneTests/VendingMachineItemTests.cs
@@ -18,7 +18,30 @@ namespace Capstone.Classes.Tests
             item = new VendingMachineItem("Potato Crisps", 3.05M, "A1");
 
             Assert.AreEqual("Crunch Crunch, Yum", item.ConsumedMessage);
+        }
 
+        [TestMethod]
+        public void ConsumedMessageTest_A_Should_Be_Crunch_Crunch_2()
+        {
+            item = new VendingMachineItem("Stackers", 1.45M, "A2");
+
+            Assert.AreEqual("Crunch Crunch, Yum", item.ConsumedMessage);
+        }
+
+        [TestMethod]
+        public void ConsumedMessageTest_B_Should_Be_Munch_Munch()
+        {
+            item = new VendingMachineItem("Moonpie", 1.80M, "B1");
+
+            Assert.AreEqual("Munch Munch, Yum", item.ConsumedMessage);
+        }
+
+        [TestMethod]
+        public void ConsumedMessageTest_B_Should_Be_Munch_Munch_2()
+        {
+            item = new VendingMachineItem("Cowtales", 1.50M, "B2");
+
+            Assert.AreEqual("Munch Munch, Yum", item.ConsumedMessage);
         }
     }
 }
